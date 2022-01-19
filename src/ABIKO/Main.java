190e7df0c6e7bcc736d33da1dae579ec9a53c01c
bam515 <imsb8415@gmail.com>
2022-01-19 15:37:13 +0900

    Main modify

--- src/ABIKO/Main.java
+++ src/ABIKO/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Main extends Application {
 	
@@ -14,7 +15,7 @@ public class Main extends Application {
 	}
 
 	@Override
-	public void start(javafx.stage.Stage primaryStage) throws Exception {
+	public void start(Stage primaryStage) throws Exception {
 		Parent root = FXMLLoader.load(getClass().getResource("orderView.fxml"));
 		Scene scene = new Scene(root);
 		scene.getStylesheets().add(getClass().getResource("main.css").toString());
