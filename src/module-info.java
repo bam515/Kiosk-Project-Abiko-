5e77dd2ae3a845c3382a169ab7dcf753cc1ed7ab
bam <devbam515@gmail.com>
2025-02-24 17:08:37 +0900

    라이브러리 추가

---
+++ src/module-info.java
@@ -0,0 +1,15 @@
+open module thisisjava_appendix_javafx {
+    requires java.se;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.media;
+    requires jfoenix;
+    requires commons.math3;
+    requires org.apache.poi.poi;
+    requires SparseBitSet;
+    requires org.apache.commons.io;
+    requires org.apache.commons.collections4;
+    requires org.apache.logging.log4j;
+    requires org.apache.logging.log4j.core;
+    requires org.apache.commons.codec;
+}
